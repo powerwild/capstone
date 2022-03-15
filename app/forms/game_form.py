e3f43ba1e9a0c0f8f4a42761088391b9eb50adb8
Casey Spears <spearscase@gmail.com>
2022-03-15 14:54:10 -0600

    finished frontend forms

--- app/forms/game_form.py
+++ app/forms/game_form.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, IntegerField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, NumberRange
 
 class GameForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     image_url = StringField('Image URL', validators=[DataRequired(), URL()])
     description = TextAreaField('Description', validators=[DataRequired()])
-    genre = SelectField('Genre', choices=[])
-    console = SelectField('Console', choices=[])
-    copies = IntegerField('Copies Owned', validators=[DataRequired()])
+    genre = SelectField('Genre', choices=['Strategy', 'Sandbox', 'First Person Shooter', 'Multiplayer Online', 'Role-Playing', 'Simulation', 'Sports', 'Puzzle', 'Adventure', 'Survival', 'Platformer'])
+    console = SelectField('Console', choices=['PlayStation 4','Playstation 3', 'Xbox 1', 'Xbox 360', 'Switch', 'Wii U', 'PC'])
+    copies = IntegerField('Copies Owned', validators=[DataRequired(), NumberRange(min=1, message='Must own at least one copy.')])
