e16cfc23bf7c12b8f6d65af1eb9d4d4d6051b6f2
Casey Spears <spearscase@gmail.com>
2022-03-22 12:30:46 -0600

    lunch break

--- app/forms/login.py
+++ app/forms/login.py
@@ -6,5 +6,5 @@ from wtforms.validators import InputRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
-    credential = StringField('Username', validators=[InputRequired()])
+    credential = StringField('Username', validators=[InputRequired(), Length(min=3, message='Credential must be at least 3 characters.')])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=6, message='Password must be at least 6 characters.')])
--- app/routes/trade_routes.py
+++ app/routes/trade_routes.py
@@ -18,7 +18,7 @@ def get_trades():
 
 @trade_routes.route('/', methods=['POST'])
 @login_required
-def create_trade(recipient_id):
+def create_trade():
     form = NewTradeForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
