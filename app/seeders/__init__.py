0a0c040b2b685f06499ff2be449db5e5b3c37e27
Casey Spears <spearscase@gmail.com>
2022-11-25 14:29:18 -0600

    finished render updates

--- app/seeders/__init__.py
+++ app/seeders/__init__.py
@@ -4,11 +4,19 @@ from app.seeders.game_seeds import add_game_seeds, remove_game_seeds
 from app.seeders.trade_seeds import add_trade_seeds, remove_trade_seeds
 from app.seeders.review_seeds import add_review_seeds, remove_review_seeds
 
+from app import db, environment, SCHEMA
+
 seeder_command = AppGroup('seed')
 
 
 @seeder_command.command('all')
 def seed_all():
+    if environment == 'production':
+        db.session.execute(f"TRUNCATE table {SCHEMA}.trades RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.reviews RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.games RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.commit()
     add_user_seeds()
     add_game_seeds()
     add_trade_seeds()
@@ -18,6 +26,6 @@ def seed_all():
 @seeder_command.command('undo')
 def unseed_all():
     remove_trade_seeds()
+    remove_review_seeds()
     remove_game_seeds()
     remove_user_seeds()
-    remove_review_seeds()
